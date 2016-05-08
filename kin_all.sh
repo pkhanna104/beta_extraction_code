671a26323f352ec52e83ab7e1426f8bba4f86058
preeya <pkhanna104@gmail.com>
2016-05-07 18:31:04 -0700

    bash script to run all knarm lfpmod sessions in a series

---
+++ kin_all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+nohup python make_pytable_lfpmod_kinarm.py 11 > nohup11.out
+sleep 5m
+nohup python make_pytable_lfpmod_kinarm.py 12 > nohup12.out
+sleep 5m
+nohup python make_pytable_lfpmod_kinarm.py 3 > nohup31.out
+sleep 5m
+nohup python make_pytable_lfpmod_kinarm.py 31 > nohup31.out
+sleep 5m
+nohup python make_pytable_lfpmod_kinarm.py 32 > nohup32.out
+sleep 5m
+nohup python make_pytable_lfpmod_kinarm.py 40 > nohup40.out
+sleep 5m
+nohup python make_pytable_lfpmod_kinarm.py 42 > nohup42.out
+sleep 5m
+nohup python make_pytable_lfpmod_kinarm.py 43 > nohup43.out 

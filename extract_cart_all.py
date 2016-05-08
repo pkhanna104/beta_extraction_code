e93a87cfc77b3e17b012d47657a3c7cc4096b552
preeya <pkhanna104@gmail.com>
2016-05-07 18:11:24 -0700

    fixed to include neural sig only

--- extract_cart_all.py
+++ extract_cart_all.py
@@ -4,18 +4,20 @@ if __name__ == '__main__':
     import sys
     arg_ind = int(sys.argv[1])
 
+
+    d = dict(neural_sig_only=True, t_range=[1, 2.5], system='nucleus', tasks=['lfp_mod_mc_reach_out'],
+        spec_method='Welch')
+
     if arg_ind == 0:
-        d = dict(behav_file_name='pap_rev_cart_behav_upside_down',\
+        d2 = dict(behav_file_name='pap_rev_cart_behav_upside_down',\
                        neural_file_name = 'pap_rev_cart_behav_upside_down_welch_neural',\
                        task_entry_dict_fname='task_entries_upside_down_cart.mat',\
-                       task_entry_dict_go = 'task_entries_upside_down_cart.mat._start_inds.mat',\
-                       t_range=[1, 2.5],\
-                       spec_method='Welch',\
-                       system='nucleus',\
-                       tasks=['lfp_mod_mc_reach_out'])
+                       task_entry_dict_go = 'task_entries_upside_down_cart.mat._start_inds.mat')
+
+        d3 = d.update(d2)
 
         import make_pytable_lfpmod_bmi3d as mp
-        lfpd = mp.lfp_task_data(**d)
+        lfpd = mp.lfp_task_data(**d3)
         lfpd.get_behavior(system='nucleus')
         lfpd.get_lfpmod_behavior(system='nucleus')
         lfpd.moving_window = [.251, .011]
@@ -27,26 +29,27 @@ if __name__ == '__main__':
     # te = [6281, 6282, 6283, 6284, 6285, 6286, 6287, 6288, 6294, 6295, 6298,
     #   6299, 6303, 6304, 6306, 6307, 6308], ntrials = [2328]
     elif arg_ind == 1:
-        d = dict(behav_file_name='pap_rev_cart_behav_targ1',\
+        d2 = dict(behav_file_name='pap_rev_cart_behav_targ1',\
                        neural_file_name = 'pap_rev_cart_behav_targ1_welch_neural',\
                        task_entry_dict_fname='task_entries_targ1_cart.mat',\
                        task_entry_dict_go = 'task_entries_targ1_cart.mat._start_inds.mat',\
-                       t_range=[1, 2.5],\
-                       spec_method='Welch',\
-                       system='nucleus',\
-                       tasks=['lfp_mod_mc_reach_out'])
+                       )
+        d3 = d.update(d2)
+
         import make_pytable_lfpmod_bmi3d as mp
-        lfpd = mp.lfp_task_data(**d)
+        lfpd = mp.lfp_task_data(**d3)
         lfpd.get_behavior(system='nucleus')
         lfpd.get_lfpmod_behavior(system='nucleus')
         lfpd.moving_window = [.251, .011]
-        kw = dict(t_range=[2.5, 1], channels=[124, 33, 1], get_neural_only=True)
+
+        kw = dict(t_range=[2.5, 1], channels=[124, 33, 1])
         lfpd.make_neural(['lfp_mod_mc_reach_out'], **kw)
 
         #For longer files: 
         lfpd.neur_fname = 'pap_rev_cart_behav_targ1_welch_neural_long_times'
         lfpd.moving_window = [1.001, .011]
-        kw = dict(t_range=[3.5, 2], channels=[124, 33, 1], get_neural_only=True)
+
+        kw = dict(t_range=[3.5, 2], channels=[124, 33, 1])
         lfpd.make_neural(['lfp_mod_mc_reach_out'], **kw)
 
    # Old files: 
@@ -62,27 +65,24 @@ if __name__ == '__main__':
     # te = [6314, 6315, 6317, 6318, 6353, 6354]
     # ntrials = 2036
     elif arg_ind == 3:
-        d = dict(behav_file_name='pap_rev_cart_behav_targ3',\
+        d2 = dict(behav_file_name='pap_rev_cart_behav_targ3',\
                        neural_file_name = 'pap_rev_cart_behav_targ3_welch_neural',\
                        task_entry_dict_fname='task_entries_targ3_cart.mat',\
                        task_entry_dict_go = 'task_entries_targ3_cart.mat._start_inds.mat',\
-                       t_range=[1, 2.5],\
-                       spec_method='Welch',\
-                       system='nucleus',\
-                       tasks=['lfp_mod_mc_reach_out'],
-                       neural_sig_only=True
                        )
+        d3 = d.update(d2)
+
         import make_pytable_lfpmod_bmi3d as mp
-        lfpd = mp.lfp_task_data(**d)
+        lfpd = mp.lfp_task_data(**d3)
         lfpd.get_behavior(system='nucleus')
         lfpd.get_lfpmod_behavior(system='nucleus')
         lfpd.moving_window = [.251, .011]
-        kw = dict(t_range=[2.5, 1], channels=[124, 33, 1], get_neural_only=True)
+        kw = dict(t_range=[2.5, 1], channels=[124, 33, 1])
         lfpd.make_neural(['lfp_mod_mc_reach_out'], **kw)
 
         lfpd.neur_fname = 'pap_rev_cart_behav_targ3_welch_neural_long_times'
         lfpd.moving_window = [1.001, .011]
-        kw = dict(t_range=[3.5, 2], channels=[124, 33, 1], get_neural_only=True)
+        kw = dict(t_range=[3.5, 2], channels=[124, 33, 1])
         lfpd.make_neural(['lfp_mod_mc_reach_out'], **kw)
 
 
@@ -93,27 +93,24 @@ if __name__ == '__main__':
     # te = [6964, 6965, 6966, 6967, 6968, 6969, 6970, 6971]
     # ntrials = 2685
     elif arg_ind == 7:
-        d = dict(behav_file_name='pap_rev_cart_behav_targ7',\
+        d2 = dict(behav_file_name='pap_rev_cart_behav_targ7',\
                        neural_file_name = 'pap_rev_cart_behav_targ7_welch_neural',\
                        task_entry_dict_fname='task_entries_targ7_cart.mat',\
                        task_entry_dict_go = 'task_entries_targ7_cart.mat._start_inds.mat',\
-                       t_range=[1, 2.5],\
-                       spec_method='Welch',\
-                       system='nucleus',\
-                       tasks=['lfp_mod_mc_reach_out'],
-                       neural_sig_only=True
                        )
+        d3 = d.update(d2)
+
         import make_pytable_lfpmod_bmi3d as mp
-        lfpd = mp.lfp_task_data(**d)
+        lfpd = mp.lfp_task_data(**d3)
         lfpd.get_behavior(system='nucleus')
         lfpd.get_lfpmod_behavior(system='nucleus')
         lfpd.moving_window = [.251, .011]
-        kw = dict(t_range=[2.5, 1], channels=[124, 33, 1], get_neural_only=True)
+        kw = dict(t_range=[2.5, 1], channels=[124, 33, 1])
         lfpd.make_neural(['lfp_mod_mc_reach_out'], **kw)
 
         lfpd.neur_fname = 'pap_rev_cart_behav_targ7_welch_neural_long_times'
         lfpd.moving_window = [1.001, .011]
-        kw = dict(t_range=[3.5, 2], channels=[124, 33, 1], get_neural_only=True)
+        kw = dict(t_range=[3.5, 2], channels=[124, 33, 1])
         lfpd.make_neural(['lfp_mod_mc_reach_out'], **kw)
 
     elif arg_ind == 10:
@@ -141,28 +138,19 @@ if __name__ == '__main__':
             p.start()
 
     elif arg_ind == 11:
-        d = dict(behav_file_name='pap_rev_cart_xy_behav',\
+        d2 = dict(behav_file_name='pap_rev_cart_xy_behav',\
                neural_file_name = 'pap_rev_cart_xy_welch_neural',\
                task_entry_dict_fname='task_entries_cart_xy.mat',\
                task_entry_dict_go = 'ttask_entries_cart_xy_GO.mat',\
-               t_range=[1, 2.5],\
-               spec_method='Welch',\
-               system='nucleus', \
-               tasks=['lfp_mod_mc_reach_out'],
-               neural_sig_only=True
                )
+        d3 = d.update(d2)
+        
         import make_pytable_reach_bmi3d as mp
-        mcd = mp.manual_control_data(**d)
+        mcd = mp.manual_control_data(**d3)
         mcd.get_behavior()
         kw = dict(t_range=[1,2.5],use_go_file=True, spec_method='Welch')
         mcd.moving_window = [.251, .011]
-        jobs = []
-        import multiprocessing
-        for tsk in mcd.tasks:
-            print 'tsk: ', tsk
-            p = multiprocessing.Process(target=mcd.make_neural,args=([tsk],),kwargs=kw)
-            jobs.append(p)
-            p.start()
+        mcd.make_neural([tsk],**kw)
 
         
 
